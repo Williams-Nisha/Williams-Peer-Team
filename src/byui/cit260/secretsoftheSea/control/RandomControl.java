9ef4e06e7b5424d1b3f1d6b620297a95bf399f4b
Lorien <Lorien@10.0.1.9>
2015-05-27 21:41:56 -0400

    Merge origin/master
    Conflicts:
    	src/secretsofthesea/SecretsoftheSea.java

--- src/byui/cit260/secretsoftheSea/control/RandomControl.java
+++ src/byui/cit260/secretsoftheSea/control/RandomControl.java
@@ -10,5 +10,27 @@ package byui.cit260.secretsoftheSea.control;
  * @author Lorien
  */
 public class RandomControl {
+
+    public int calCoins(int gold, int silver, int copper){
+
+	if (gold < 0) {
+		return -1;
+        }    
+        if (silver < 0) {
+		return -1;
+        }    
+        if (copper < 0) {
+		return -1;
+        }    
+
+	int total = gold + silver + copper;
+
+	return total;
+    
+    }
+    
+    public RandomControl() {
+    }
     
 }
+
