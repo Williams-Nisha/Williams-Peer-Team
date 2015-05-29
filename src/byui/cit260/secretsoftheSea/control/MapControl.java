ee11c0fb9899823b6f9f6e125b37c599314898dd
Nisha <Nisha@192.168.1.102>
2015-05-29 16:44:07 -0600

    Merge origin/master
    Conflicts:
    	nbproject/project.properties

--- src/byui/cit260/secretsoftheSea/control/MapControl.java
+++ src/byui/cit260/secretsoftheSea/control/MapControl.java
@@ -10,5 +10,26 @@ package byui.cit260.secretsoftheSea.control;
  * @author Lorien
  */
 public class MapControl {
-    
+
+    public boolean intTrade(String[] name, double[] weight) {
+
+        if (weight[0] < 0) {
+            System.out.println("Sorry, you do not have enough" + name[0] + " to trade");
+            return false;
+        }
+        if (weight[1] < 25) {
+            System.out.println("Sorry, you do not have enough" + name[1] + " to trade");
+            return false;
+        }
+        if (weight[2] < 10) {
+            System.out.println("Sorry, you do not have enough" + name[2] + " to trade");
+            return false;
+        }
+        if (weight[3] < 30) {
+            System.out.println("Sorry, you do not have enough" + name[3] + " to trade");
+            return false;
+        }
+        System.out.println("What would you like to trade?");
+        return true;
+    }
 }
---
+++ test/byui/cit260/secretsoftheSea/control/MapControlTest.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.secretsoftheSea.control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Nisha
+ */
+public class MapControlTest {
+
+    public MapControlTest() {
+    }
+
+    /**
+     * Test of intTrade method, of class MapControl.
+     */
+    @Test
+    public void testIntTrade() {
+        System.out.println("intTrade");
+        String[] name = {"water", "food", "munition", "fuel"};
+        double[] weight = {400, 1200, 800, 300};
+        MapControl instance = new MapControl();
+        boolean expResult = true;
+        boolean result = instance.intTrade(name, weight);
+        assertEquals(expResult, result);
+        //Test 2
+        System.out.println("intTrade");
+        weight[0] = -1;
+        expResult = false;
+        result = instance.intTrade(name, weight);
+        assertEquals(expResult, result);
+        //Test 3
+        System.out.println("intTrade");
+        weight[0] = 400;
+        weight[1] = 24;
+        expResult = false;
+        result = instance.intTrade(name, weight);
+        assertEquals(expResult, result);
+        //Test 4
+        System.out.println("intTrade");
+        weight[1] = 1200;
+        weight[2] = 9;
+        expResult = false;
+        result = instance.intTrade(name, weight);
+        assertEquals(expResult, result);
+        //Test 5
+        System.out.println("intTrade");
+        weight[2] = 800;
+        weight[3] = 15;
+        expResult = false;
+        result = instance.intTrade(name, weight);
+        assertEquals(expResult, result);
+        //Test 6 Boundaries
+        System.out.println("intTrade");
+        weight[3] = 30;
+        expResult = true;
+        result = instance.intTrade(name, weight);
+        assertEquals(expResult, result);
+        //Test 7 Boundaries
+        System.out.println("intTrade");
+        weight[3] = 30;
+        weight[0]= 10;
+        expResult = true;
+        result = instance.intTrade(name, weight);
+        assertEquals(expResult, result);
+    }
+
+}
