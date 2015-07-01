d53dc8afc7d524ad881dddbc5f821515fa329de0
Lorien <Lorien@10.0.1.10>
2015-07-01 19:52:40 -0400

    Merge origin/master
    Conflicts:
    	src/citbyui/cit260secretsoftheSea/view/HelpMenuView.java

--- src/byui/cit260/secretsoftheSea/control/RandomControl.java
+++ src/byui/cit260/secretsoftheSea/control/RandomControl.java
@@ -5,34 +5,28 @@
  */
 package byui.cit260.secretsoftheSea.control;
 
+import byui.cit260.secretsoftheSea.exceptions.RandomControlException;
 /**
  *
  * @author Lorien
  */
 public class RandomControl {
 
-    public int calCoins(int [] coins){ 
-        
+    public int calCoins(int[] coins) throws RandonControlException {
+
         for (int i = 0; i < coins.length; i++) {
-        if (coins[i] < 0) {
-            return -1;
-        }
-        if (coins[i] > 75) {
-            return -1;
-        }
-        }
-        int sum = 0;
-        for (int cs: coins) {
-            sum += cs;
+            if (coins[i] < 0) {
+                throw new RandonControlException("Coins cannot be negative.");
+            }
+            if (coins[i] > 75) {
+                throw new RandonControlException("Coins cannot be greater than 75.");
+            }
+            int sum = 0;
+            for (int cs : coins) {
+                sum += cs;
+
+            }
+            System.out.println("The total coins equals " + sum);
+            return sum;
 
         }
-        System.out.println("The total coins equals " + sum);
-        return sum;
-    
-    }
-    
-    public RandomControl() {
-    }
-    
-}
-
--- src/byui/cit260/secretsoftheSea/control/ShipControl.java
+++ src/byui/cit260/secretsoftheSea/control/ShipControl.java
@@ -13,34 +13,33 @@ import byui.cit260.secretsoftheSea.exceptions.ShipControlException;
  */
 public class ShipControl {
 
-    
-    public double calStorage(double height, double width, double length) throws ShipControlException{
+    public double calStorage(double height, double width, double length) throws ShipControlException {
 
-	if (length < 5 || length > 45) {
-		throw new ShipControlException("Cannot have a negative length.");
-        }
-        
-	if (width < 5 || width > 20) {
-		throw new ShipControlException("Cannot have a negative width.");
+        if (length < 5 || length > 45) {
+            throw new ShipControlException("Cannot have a negative length.");
         }
 
-	if (height < 2 || height > 10) {
-		throw new ShipControlException("Cannot have a negative height.");
+        if (width < 5 || width > 20) {
+            throw new ShipControlException("Cannot have a negative width.");
         }
 
-	double area = length * width * height;
+        if (height < 2 || height > 10) {
+            throw new ShipControlException("Cannot have a negative height.");
+        }
 
-	return area;
+        double area = length * width * height;
 
+        return area;
 
-}
+    }
 
     public ShipControl() {
-        
+
     }
-    public boolean checkLaunch(){
+
+    public boolean checkLaunch() {
         System.out.println("Check launch status.");
         return false;
     }
-    
+
 }
